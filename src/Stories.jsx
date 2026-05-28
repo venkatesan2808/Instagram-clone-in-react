3986593612a4ab361b43d433dbf54edd091ec795
venkatesan2808 <venkatvenkat9068@gmail.com>
2026-05-28 21:22:27 +0530

    update project

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Stories() {
-  return (
-    <div className='story bg-info'>
-       Stories
+
+  const [Stories, setStories] = useState([]);
+
+  const navigate = useNavigate()
+
+  let tot =0;
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data => data.json())
+    .then (data => setStories(data))
+    .catch(err => console.log(err))
+  }, [])
+
+    return (
+    <div className='story d-flex'>
+     <div className='d-none'>
+      {tot= Stories.length}
+     </div>
+      
+       {Stories.length > 0 ? (
+         Stories.map((story)=>(
+          <div key={story.id} className='mx-1' onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
+            <div className='gradient-border'>
+               <img src={story.profilePic} alt="dp" className='story-dp rounded-circle '/>
+            </div>
+
+            <p className='text-truncate'style={{width:"50px"}}>{story.username}</p>
+          </div>
+         ))
+       ):(
+         <p>loading</p>
+       )}
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import ViewStory from './ViewStory.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path:'/',
+      element:<App/>
+    },
+    {
+      path:'/story/:id/:tot',
+      element:<ViewStory/>
+    }
+  ]
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
